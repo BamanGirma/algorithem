e06e8c06ac1b0be67f286e765088ee2bba86c904
BamanGirma <amanuelgirma360@gmail.com>
2024-06-19 21:41:20 -0800

    first edition

--- js/script.js
+++ js/script.js
@@ -123,14 +123,14 @@ function will print "Not ok"
 // console.log(res);
 
 /* 
-Write a function that takes a positive integer and returns the factorial of the number.
+Write a function that takes a positive integer and returns the factorial of the number.\
 Notes: The factorial of 0 is 1. Ex: factorial seven is : 1 × 2 × 3 × 4 × 5 × 6 × 7. The
 factorial of any positive integer x is x * (x - 1) * (x - 2) * . . . . . . * 1 (ex: factorial of 4 is
 4 * 3 * 2 * 1 = 24)
 ○ Test 1: returnFactorial(5) outputs 120
 ○ Test 2: returnFactorial(6) outputs 720
 ○ Test 3: returnFactorial(0) outputs 1 */
-/* function returnFactorial(num){
+function returnFactorial(num){
     if(num<0){
         return "number must be postive"
     }
@@ -140,7 +140,7 @@ factorial of any positive integer x is x * (x - 1) * (x - 2) * . . . . . . * 1 (
     }
     return result;
 }
-console.log(returnFactorial(5)); */
+console.log(returnFactorial(5));
 
 /* 
 A Meera array is defined to be an array containing only numbers as its elements and for
